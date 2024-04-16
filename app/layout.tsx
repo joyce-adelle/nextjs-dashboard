a063f30272802a4437da6e3244131d194d54e748
Joyce Adelusi <jadelusi11@gmail.com>
2024-04-16 21:55:22 +0100

    optimizing fonts

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,7 @@
 import '@/app/ui/global.css';
 
+import { inter } from '@/app/ui/fonts';
+
 export default function RootLayout({
   children,
 }: Readonly<{
@@ -7,7 +9,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className={inter.className}>{children}</body>
     </html>
   );
 }
