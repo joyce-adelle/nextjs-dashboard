9f7a24376722f9f7cbd981b7309ab7b43fad8aad
Joyce Adelusi <jadelusi11@gmail.com>
2024-05-13 15:24:03 +0100

    debouncing

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -3,13 +3,14 @@
 import { usePathname, useRouter, useSearchParams } from 'next/navigation';
 
 import { MagnifyingGlassIcon } from '@heroicons/react/24/outline';
+import { useDebouncedCallback } from 'use-debounce';
 
 export default function Search({ placeholder }: { placeholder: string }) {
   const searchParams = useSearchParams();
   const pathname = usePathname();
   const { replace } = useRouter();
 
-  function handleSearch(term: string) {
+  const handleSearch = useDebouncedCallback((term: string) => {
     const params = new URLSearchParams(searchParams);
 
     if (term) {
@@ -19,7 +20,7 @@ export default function Search({ placeholder }: { placeholder: string }) {
     }
 
     replace(`${pathname}?${params.toString()}`);
-  }
+  }, 300);
 
   return (
     <div className="relative flex flex-1 flex-shrink-0">
